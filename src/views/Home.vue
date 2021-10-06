64ef93ecfc0acb4dbc043c6562844e9624d6b20a
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-06 22:06:40 +0200

    :lipstick: Home.vue: replaced some default tiles with custom ones.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="tile-group">
     <tile tile-title="Speiseplan" tile-route-to="Mensa" />
-    <tile tile-title="Termine" tile-route-to="Calendar" deactivated />
-    <tile-drucker-konto tile-title="Guthaben" />
-    <tile tile-title="Noten" tile-route-to="Grades" deactivated />
+    <tile-termine />
+    <tile-drucker-konto />
+    <tile tile-title="Noten" tile-route-to="Noten" deactivated />
     <tile tile-title="Endlicht" tile-route-to="Endlicht" deactivated />
-    <tile tile-title="Einloggen" tile-route-to="Login" inverted />
+    <tile-login />
   </div>
 </template>
 
 <script>
 import TileDruckerKonto from "@/components/TileDruckerKonto";
+import TileLogin from "@/components/TileLogin";
+import TileTermine from "@/components/TileTermine";
 
 export default {
   name: "Home",
   components: {
     TileDruckerKonto,
+    TileLogin,
+    TileTermine,
   },
 };
 </script>
