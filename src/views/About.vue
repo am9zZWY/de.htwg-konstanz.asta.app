3babf86a02c3cf2c633b01ecc2eafbcf9e82dc15
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-06 23:19:17 +0200

    :sparkles: added About.vue: information about me.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="tile-group">
+    <tile
+      tile-title="Hallo"
+      tile-subtitle="Ich bin Josef. Der Entwickler von HTWG App."
+    />
+    <tile inverted>
+      Wenn du mehr über mich und meine Projekte erfahren möchtest, schaue auf
+      <strong>github.com/jpkmiller</strong> vorbei.
+    </tile>
+    <tile>
+      Wenn dir diese App gefällt, dann schreibe mir gerne eine E-Mail an
+      <strong>jo391mue@htwg-konstanz.de</strong>.
+    </tile>
   </div>
 </template>
