8a795d4b6c40bc590e9397f820d501275d3ff9b9
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-06 22:03:11 +0200

    :sparkles: TileTermine.vue.vue: added Tile for Termine.

---
+++ src/components/TileTermine.vue
@@ -0,0 +1,42 @@
+<template>
+  <tile
+    big-content
+    tile-route-to="Termine"
+    tile-title="Termine"
+    :tile-subtitle="weekday"
+  >
+    {{ date }}
+  </tile>
+</template>
+
+<script>
+export default {
+  name: "TileTermine",
+  data() {
+    return {
+      weekdays: [
+        "Montag",
+        "Dienstag",
+        "Mittwoch",
+        "Donnerstag",
+        "Freitag",
+        "Samstag",
+        "Sonntag",
+      ],
+    };
+  },
+  computed: {
+    today: function () {
+      return new Date();
+    },
+    date: function () {
+      return `${this.today.getDate()}.${
+        this.today.getMonth() + 1
+      }.${this.today.getFullYear()}`;
+    },
+    weekday: function () {
+      return this.weekdays[this.today.getDay() - 1];
+    },
+  },
+};
+</script>
