77dce554b94a15239a50a51d48a86ffe71f699c1
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-06 22:03:00 +0200

    :sparkles: TileLogin.vue: added Tile for login.

---
+++ src/components/TileLogin.vue
@@ -0,0 +1,25 @@
+<template>
+  <tile
+    tile-title="Einloggen"
+    :tile-subtitle="subTitle"
+    tile-route-to="Login"
+    inverted
+  />
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "TileLogin",
+  computed: {
+    ...mapState(["username"]),
+    subTitle: function () {
+      if (this.username !== "" || this.username !== null) {
+        return `Hallo, ${this.username}!`;
+      }
+      return "Logge dich ein, um weitere Dienste zu verwenden";
+    },
+  },
+};
+</script>
