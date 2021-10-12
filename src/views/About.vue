abe28ea82fb3e6f7ff74144d71106ec64e88d345
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-12 22:36:07 +0200

    :lipstick: About.vue: added link to GitHub.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="tile-group">
     <tile
+      tile-subtitle="Ich bin Josef. Der Entwickler der HTWG App."
       tile-title="Hallo"
-      tile-subtitle="Ich bin Josef. Der Entwickler von HTWG App."
     />
-    <tile inverted>
+    <tile inverted tile-type="div">
       Wenn du mehr über mich und meine Projekte erfahren möchtest, schaue auf
-      <strong>github.com/jpkmiller</strong> vorbei.
+      <a href="https://github.com/jpkmiller" rel="nofollow" target="_blank" class="link">
+        <strong>github.com/jpkmiller</strong>
+      </a> vorbei.
     </tile>
     <tile>
       Wenn dir diese App gefällt, dann schreibe mir gerne eine E-Mail an
