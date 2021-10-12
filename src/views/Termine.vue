250a13aa7271b5a4b28d334e1e602c34e100e978
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-12 22:32:59 +0200

    :bug: Termine.vue: works now better. - replaced iframe with fetch and v-html. - fixed links.

--- src/views/Termine.vue
+++ src/views/Termine.vue
@@ -1,7 +1,35 @@
 <template>
-  <iframe
-    class="termine"
-    src="https://htwg-app-back.herokuapp.com/?termine"
-    title="Termine und Fristen"
-  />
+  <div class="termine" id="termine">
+    <div v-html="html" />
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Termine",
+  data() {
+    return {
+      html: "",
+    };
+  },
+  methods: {
+    /**
+     * Modifies links so that they start from the official page of the HTWG
+     */
+    fixLinks: function () {
+      const termine = document.getElementById("termine");
+      let anchors = Array.from(termine.getElementsByTagName("a"));
+      anchors.forEach((a) => {
+        a.href = "https://www.htwg-konstanz.de" + a.pathname;
+        a.target = "_blank";
+      });
+    },
+  },
+  mounted() {
+    fetch("https://htwg-app-back.herokuapp.com/?termine")
+      .then((response) => response.text())
+      .then((text) => (this.html = text))
+      .then(() => this.fixLinks());
+  },
+};
+</script>
