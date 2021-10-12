bd781c9bd748c68146d0cd2cb25b1d4748863c06
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-12 22:35:02 +0200

    :lipstick: Home.vue: changed order of tiles.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="tile-group">
     <tile tile-title="Speiseplan" tile-route-to="Mensa" />
-    <tile-termine />
-    <tile-drucker-konto />
+    <tile tile-title="Stundenplan" tile-route-to="Stundenplan" deactivated />
+    <tile tile-title="Campus Plan" tile-route-to="CampusPlan" />
     <tile tile-title="Noten" tile-route-to="Noten" />
+    <tile-drucker-konto />
+    <tile-termine />
     <tile tile-title="Endlicht" tile-route-to="Endlicht" deactivated />
     <tile-login />
-    <tile tile-title="Campus Plan" tile-route-to="CampusPlan" />
+    <tile tile-title="Über" tile-route-to="About" />
   </div>
 </template>
 
