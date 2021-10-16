17c4c711cbfc3d5aa52c4c0f24e5f392fc2e3a7b
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-16 15:29:25 +0200

    🚸 added Health.vue.

---
+++ src/views/Health.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tile-group">
+    <tile tile-subtitle="Du solltest ungefähr 1,5 bis 2,5 Liter am Tag trinken.">
+      Hast du heute schon <strong>Wasser getrunken?</strong><br />
+    </tile>
+    <tile
+      inverted
+      tile-icon="ri-time-line"
+      tile-subtitle="Pausen sind wichtig, um die Konzentration aufzubauen."
+      tile-title="Lege auch Mal eine Pause ein, wenn du Lernst."
+    />
+    <tile
+      tile-icon="ri-windy-line"
+      tile-subtitle="Wieder ein... und wieder aus."
+      tile-title="Tief einatmen! Und wieder aus..."
+    />
+    <tile tile-title="Achte auf dich!">Überarbeite dich nicht.</tile>
+    <tile inverted tile-title="Dir geht es nicht gut?">
+      <ul class="tile-list">
+        <li>
+          <a href="https://seezeit.com/beratung/psychotherapeutische-beratung/" rel="nofollow" target="_blank">Psychotherapeutische
+            Beratung</a>
+        </li>
+        <li>
+          <a href="https://www.nightline-konstanz.de/kontakt.html">Nightline</a>
+        </li>
+      </ul>
+    </tile>
+  </div>
+</template>
