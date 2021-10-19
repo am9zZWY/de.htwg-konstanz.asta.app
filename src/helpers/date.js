6580a547b8219b56a91626428dade3af23e99184
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-19 22:19:13 +0200

    📝 :sparkles: date.js: added more helpers and more JDOC.

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,5 +1,15 @@
+export function createDate(surpassWeekend = false) {
+  let d = new Date();
+  let dAsNumber = dayAsNumber(d);
+  if (dAsNumber >= 5 && surpassWeekend) {
+    d.setDate(d.getDate() + (7 - dAsNumber));
+  }
+  return d;
+}
+
 /**
  * https://stackoverflow.com/a/27323864
+ * Round milliseconds of date to 00:00:00.
  * @param timeStamp
  * @returns {number}
  */
@@ -9,6 +19,23 @@ export function roundDate(timeStamp) {
   return new Date(timeStamp).getTime();
 }
 
+/**
+ * Add days to Date.
+ * @param d
+ * @param days
+ * @returns {*}
+ */
+export function addDay(d, days) {
+  let _d = new Date(d.getTime());
+  _d.setDate(_d.getDate() + days);
+  return _d;
+}
+
+/**
+ * Convert milliseconds to seconds.
+ * @param timestamp
+ * @returns {number}
+ */
 export function toSeconds(timestamp) {
   return Math.round(timestamp / 1000);
 }
@@ -23,10 +50,36 @@ const weekdays = [
   "Sonntag",
 ];
 
-export function date(d) {
+/**
+ * Format Date as String.
+ * @param d
+ * @returns {string}
+ */
+export function formatDate(d) {
   return `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()}`;
 }
 
+/**
+ * Return Weekday of Date as String.
+ * @param d
+ * @returns {string}
+ */
 export function weekday(d) {
-  return weekdays[d.getDay() - 1];
+  return weekdays[dayAsNumber(d)];
+}
+
+/**
+ * Get Day starting from 0.
+ * If Saturday or Sunday set to next Monday.
+ *
+ * @param {Date} d
+ * @param {boolean} surpassWeekend
+ * @returns {number}
+ */
+export function dayAsNumber(d, surpassWeekend = false) {
+  let day = (d.getDay() - 1 + 7) % 7;
+  if (surpassWeekend && day >= 5) {
+    day = 7;
+  }
+  return day;
 }
