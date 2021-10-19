8f7c4a28727e130442f5cfe26b1dc93ec555cf9c
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-19 22:19:58 +0200

    🥅 fetchData.js: added catch and more flexible options.

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -6,15 +6,32 @@
  * following options are available
  *   - {string} body
  */
-export default function fetchData(callback, options = {}) {
-  const { method, body } = options || {};
-
-  fetch(`https://htwg-app-back.herokuapp.com/`, {
-    method: method || "POST",
-    body: body || "",
-  }).then((result) => {
-    if (result.status === 200) {
-      callback(result);
+export default function fetchData(callback, options = null) {
+  let method, body, requestBody;
+  let address = "https://htwg-app-back.herokuapp.com/";
+  if (options !== null) {
+    if (typeof options === "object") {
+      ({ method, body } = options || {});
+      requestBody = {
+        method: method || "POST",
+        body: body || "",
+      };
+    } else if (typeof options === "string") {
+      address = options;
     }
-  });
+  }
+
+  fetch(address, requestBody)
+    .then((result) => {
+      if (result.status === 200) {
+        callback(result);
+      }
+    })
+    .catch(() => {
+      console.warn(
+        `There was an error when trying to fetch from ${address} with params ${
+          requestBody || ""
+        }`
+      );
+    });
 }
