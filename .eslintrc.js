6533392493b31c96f03835449fd66a737c44a5c0
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-27 12:10:42 +0200

    🔧 .eslintrc.js: added typescript parser and extended typescript.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 module.exports = {
   root: true,
-
   env: {
     node: true,
   },
-
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@typescript-eslint/parser",
   },
-
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -23,5 +18,11 @@ module.exports = {
         jest: true,
       },
     },
+  ],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "@vue/typescript",
   ],
 };
