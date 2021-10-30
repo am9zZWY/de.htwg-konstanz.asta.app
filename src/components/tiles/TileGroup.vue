89541954c90b4d6996b1cffded03bc3c2ea1ac9e
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-30 17:25:07 +0200

    :sparkles: added TileGroup.vue: replaces `<div class="tile-group"></...>`.
    Has advantage of handling errors of multiple tiles directly.

---
+++ src/components/tiles/TileGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tile-group">
+    <template v-if="!error">
+      <slot></slot>
+    </template>
+    <template v-else>
+      <tile :tile-subtitle="tileTitle" :status="status" />
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TileGroup",
+  props: {
+    status: {
+      type: Number,
+      required: false,
+      default: 200,
+    },
+    tileTitle: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  computed: {
+    error: function () {
+      return this.status !== 200;
+    },
+  },
+};
+</script>
