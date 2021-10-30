cb0d32ff2cda8f424e5425a47d7249ff0c5ebf8e
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-10-30 17:21:26 +0200

    :sparkles: About.vue: added contributors.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="tile-group">
-    <tile
-      tile-subtitle="Ich bin Josef. Der Entwickler der HTWG App."
-      tile-title="Hallo"
-    />
-    <tile is-inverted tile-type="div">
-      Wenn du mehr über mich und meine Projekte erfahren möchtest, schaue auf
-      <a
-        href="https://github.com/jpkmiller"
-        rel="nofollow"
-        target="_blank"
-        class="link"
-      >
-        <strong>github.com/jpkmiller</strong>
-      </a>
-      vorbei.
+    <tile tile-title="Hallo">
+      Folgende Personen haben an diesem Projekt mitgewirkt:
+      <ul>
+        <li
+          v-for="(contributor, contributor_index) in contributors"
+          :key="contributor_index"
+        >
+          {{ contributor.name }}
+          <br />
+        </li>
+      </ul>
     </tile>
-    <tile>
-      Wenn dir diese App gefällt, dann schreibe mir gerne eine E-Mail an
-      <strong>jo391mue@htwg-konstanz.de</strong>.
+    <tile is-inverted>
+      Wenn dir diese App gefällt, dann schreibe uns gerne eine E-Mail an:<br />
+      <strong class="email">jo391mue at htwg-konstanz.de</strong>.
     </tile>
   </div>
 </template>
+
+<script>
+const contributors = require("@/assets/contributors.json");
+
+export default {
+  name: "About",
+  data() {
+    return {
+      contributors,
+    };
+  },
+};
+</script>
