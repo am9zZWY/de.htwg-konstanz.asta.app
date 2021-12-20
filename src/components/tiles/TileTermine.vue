3fa0f9e061170ec775440ebbfd944987d755a9cd
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-12-20 22:34:57 +0100

    :truck: TileTermine.vue: updated path of `dateHelper.ts`.

--- src/components/tiles/TileTermine.vue
+++ src/components/tiles/TileTermine.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import { weekday, formatDate } from "@/helpers/dateHelper";
+import { weekday, formatDate } from "@/helpers/dateHelper.ts";
 
 export default {
   name: "TileTermine",
