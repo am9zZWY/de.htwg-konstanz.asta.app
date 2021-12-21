636448d6a5a936ebf94f1c23581eba3acd29428f
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-12-21 16:30:30 +0100

    :sparkles: post.ts: added helper for post requests.

---
+++ src/helpers/post.ts
@@ -0,0 +1,24 @@
+import { Store, useStore } from "vuex";
+import { computed } from "vue";
+import { post } from "@/helpers/fetchData";
+
+export default async function get_content_via_post(
+  reqtype: string,
+  store: Store<any> = useStore()
+): Promise<{
+  content: any;
+  status: number;
+}> {
+  const username = computed(() => store?.state?.username);
+  const password = computed(() => store?.state?.password);
+
+  const body = JSON.stringify({
+    username: username.value || "",
+    password: password.value || "",
+    reqtype: reqtype,
+  });
+
+  console.log(body);
+
+  return await post(body);
+}
