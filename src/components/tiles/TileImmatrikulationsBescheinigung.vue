ae815bdf7bfb7d1423828001bb1c3286d843c844
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2021-12-25 00:35:07 +0100

    :art: TileImmatrikulationsBescheinigung.vue: format.

--- src/components/tiles/TileImmatrikulationsBescheinigung.vue
+++ src/components/tiles/TileImmatrikulationsBescheinigung.vue
@@ -9,13 +9,13 @@
 
 <script lang="ts">
 import Tile from "@/components/tiles/Tile.vue";
-import {useStore} from "vuex";
-import {computed, ref} from "vue";
-import {raw_post} from "@/helpers/fetchData";
+import { useStore } from "vuex";
+import { computed, ref } from "vue";
+import { raw_post } from "@/helpers/fetchData";
 
 export default {
   name: "TileImmatrikulationsBescheinigung",
-  components: {Tile},
+  components: { Tile },
   setup() {
     const store = useStore();
     const wait = ref(false);
@@ -31,17 +31,17 @@ export default {
       });
 
       raw_post(body)
-          .then((result) => result.blob())
-          .then((blob) => {
-            wait.value = false;
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = url;
-            a.download = "Immatrikulations_Bescheinigung.pdf";
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-          });
+        .then((result) => result.blob())
+        .then((blob) => {
+          wait.value = false;
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = "Immatrikulations_Bescheinigung.pdf";
+          document.body.appendChild(a);
+          a.click();
+          a.remove();
+        });
     };
     return {
       wait,
