319aff8ee4782a78820acd8ba5893c71b6362bb1
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-01-29 22:14:48 +0000

    Deploying to page from @ HTWG-App/htwg-app-front@c5aabc08485dad118c7d03cee805c594e49e0fbf 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/htwg-app-front/precache-manifest.6e3c61236d2bfd9f1e8215f751d7355f.js"
+  "/htwg-app-front/precache-manifest.5b44958302f26dda53b338d1e29f00c3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "htwg-app-front"});
