975cf051c68cc40d03ca77a37cf7c6f8f3e6cbcd
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-10 19:41:09 +0100

    :truck: router.ts: updated paths.

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory, Router } from "vue-router";
-import Home from "@/views/Home.vue";
-import Canteen from "@/views/Canteen.vue";
-import Timetable from "@/views/Timetable.vue";
+import Home from "@/views/View_Home.vue";
+import Canteen from "@/views/api_relevant/general/View_Canteen.vue";
+import Timetable from "@/views/api_relevant/user_related/View_Timetable.vue";
 
 const routes = [
   {
@@ -24,49 +24,61 @@ const routes = [
     path: "/Health",
     name: "Health",
     component: () =>
-      import(/* webpackChunkName: "health" */ "@/views/Health.vue"),
+      import(/* webpackChunkName: "health" */ "@/views/View_Health.vue"),
   },
   {
     path: "/Termine",
     name: "Termine",
     component: () =>
-      import(/* webpackChunkName: "termine" */ "@/views/Termine.vue"),
+      import(
+        /* webpackChunkName: "termine" */ "@/views/api_relevant/general/View_UniversityEvents.vue"
+      ),
   },
   {
     path: "/Grades",
     name: "Grades",
     component: () =>
-      import(/* webpackChunkName: "grades" */ "@/views/Grades.vue"),
+      import(
+        /* webpackChunkName: "grades" */ "@/views/api_relevant/user_related/View_Grades.vue"
+      ),
   },
   {
     path: "/Endlicht",
     name: "Endlicht",
     component: () =>
-      import(/* webpackChunkName: "endlicht" */ "@/views/Endlicht.vue"),
+      import(
+        /* webpackChunkName: "endlicht" */ "@/views/api_relevant/general/View_Endlicht.vue"
+      ),
   },
   {
     path: "/Library",
     name: "Library",
     component: () =>
-      import(/* webpackChunkName: "library" */ "@/views/Library.vue"),
+      import(
+        /* webpackChunkName: "library" */ "@/views/api_relevant/general/View_Library.vue"
+      ),
   },
   {
     path: "/CampusPlan",
     name: "CampusPlan",
     component: () =>
-      import(/* webpackChunkName: "campus-plan" */ "@/views/CampusPlan.vue"),
+      import(
+        /* webpackChunkName: "campus-plan" */ "@/views/View_CampusPlan.vue"
+      ),
   },
   {
     path: "/Login",
     name: "Login",
     component: () =>
-      import(/* webpackChunkName: "login" */ "@/views/Login.vue"),
+      import(
+        /* webpackChunkName: "login" */ "@/views/api_relevant/user_related/View_Login.vue"
+      ),
   },
   {
     path: "/About",
     name: "About",
     component: () =>
-      import(/* webpackChunkName: "about" */ "@/views/About.vue"),
+      import(/* webpackChunkName: "about" */ "@/views/View_About.vue"),
   },
 ];
 
