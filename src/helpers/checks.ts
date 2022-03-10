9f09cdd7425d482bc49439ff673af0ff28fb198e
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-10 14:24:35 +0000

    Deploying to page from @ HTWG-App/htwg-app-front@88480a49b3f5e5a9c35c9f335a6f89eccce4c2a9 🚀

--- src/helpers/checks.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @description Checks if item is null or undefined.
- * @param item
- */
-export function nullOrUndefined(item: any): item is null | undefined {
-  return item === null || typeof item === "undefined";
-}
