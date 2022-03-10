95f8a5c1095ec97ea4aad3081a8786c847f66550
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-10 13:13:35 +0100

    main.ts: removed globally imported components.

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,8 @@ import App from "@/App.vue";
 import router from "@/router/router";
 import { store } from "@/store/store";
 
-/* import components */
-import Tile from "@/components/tiles/Tile.vue";
-import TileWait from "@/components/tiles/TileWait.vue";
-
 /* init app */
 const app = createApp(App);
-
-/* register global components */
-app.component("Tile", Tile);
-app.component("TileWait", TileWait);
 
 /* register store and router */
 app.use(store);
