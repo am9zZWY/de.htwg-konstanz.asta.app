12cd1216f8ba4438598735f861e2482891f50c28
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-03-10 13:12:57 +0100

    encryptionHelper.ts: made sure that function returns only string.

--- src/helpers/encryptionHelper.ts
+++ src/helpers/encryptionHelper.ts
@@ -5,12 +5,16 @@ import { JSEncrypt } from "jsencrypt";
  * Encrypts data with RSA encryption.
  * @param {string} data
  */
-export default function encrypt_data(data: string) {
+export default function encrypt_data(data: string): string {
   const jsencrypt = new JSEncrypt();
   if (process.env.NODE_ENV === "development") {
     jsencrypt.setPublicKey(key.pubKeyDev);
   } else {
     jsencrypt.setPublicKey(key.pubKey);
   }
-  return jsencrypt.encrypt(data);
+  const encryptedData = jsencrypt.encrypt(data);
+  if (!encryptedData) {
+    return "";
+  }
+  return encryptedData;
 }
